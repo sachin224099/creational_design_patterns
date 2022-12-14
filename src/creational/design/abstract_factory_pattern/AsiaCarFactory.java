2bcae44c852f2922f07fa88f5541e5804e890bb8
sachin <sachin224099@gmail.com>
2022-12-14 23:26:31 +0530

    creation design patterns

---
+++ src/creational/design/abstract_factory_pattern/AsiaCarFactory.java
@@ -0,0 +1,33 @@
+package creational.design.abstract_factory_pattern;
+
+import creational.design.factory_pattern.CarType;
+import creational.design.abstract_factory_pattern.LuxuryCar;
+import creational.design.abstract_factory_pattern.SedanCar;
+import creational.design.abstract_factory_pattern.SmallCar;
+
+public class AsiaCarFactory
+{
+  public static Car buildCar(CarType model)
+  {
+    Car car = null;
+    switch (model)
+    {
+      case SMALL:
+      car = new SmallCar(Location.ASIA);
+      break;
+ 
+      case SEDAN:
+      car = new SedanCar(Location.ASIA);
+      break;
+ 
+      case LUXURY:
+      car = new LuxuryCar(Location.ASIA);
+      break;
+ 
+      default:
+      //throw some exception
+      break;
+    }
+    return car;
+  }
+}
